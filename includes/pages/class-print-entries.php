b169305942f3e4609002ca1a1b3264230402b521
Yoren Chang <yorenchang@gmail.com>
2018-07-15 03:38:28 +0800

    Fix RTL CSS issues (#144)

--- includes/pages/class-print-entries.php
+++ includes/pages/class-print-entries.php
@@ -32,7 +32,7 @@ class Gravity_Flow_Print_Entries {
 		self::get_header( $entry_ids );
 
 		?>
-		<body>
+		<body<?php echo ( is_rtl() ) ? ' class="rtl"' : ''; ?>>
 
 		<div id="view-container">
 			<?php
@@ -172,7 +172,7 @@ class Gravity_Flow_Print_Entries {
 
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-		<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+		<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"<?php echo ( is_rtl() ) ? ' dir="rtl"' : ''; ?>>
 		<head>
 			<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
 			<meta name="keywords" content=""/>
