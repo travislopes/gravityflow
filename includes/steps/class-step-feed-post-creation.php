a82bf121ff3d7421982177b800bb105d9467485f
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-11-10 09:39:37 +0000

    Updated post creation step

--- includes/steps/class-step-feed-post-creation.php
+++ includes/steps/class-step-feed-post-creation.php
@@ -21,6 +21,8 @@ class Gravity_Flow_Step_Feed_Post_Creation extends Gravity_Flow_Step_Feed_Add_On
 	/**
 	 * The step type.
 	 *
+	 * @since 1.4.3
+	 *
 	 * @var string
 	 */
 	public $_step_type = 'post_creation';
@@ -28,17 +30,37 @@ class Gravity_Flow_Step_Feed_Post_Creation extends Gravity_Flow_Step_Feed_Add_On
 	/**
 	 * The name of the class used by the add-on.
 	 *
+	 * @since 2.3.4 Changed from GF_Post_Creation.
+	 * @since 1.4.3
+	 *
 	 * @var string
 	 */
-	protected $_class_name = 'GF_Post_Creation';
+	protected $_class_name = 'GF_Advanced_Post_Creation';
 
 	/**
 	 * Returns the step label.
+	 *
+	 * @since 1.4.3
 	 *
 	 * @return string
 	 */
 	public function get_label() {
 		return 'Post Creation';
+	}
+
+	/**
+	 * Returns the class name for the add-on.
+	 *
+	 * @since 2.3.4
+	 *
+	 * @return string
+	 */
+	public function get_feed_add_on_class_name() {
+		if ( class_exists( 'GF_Post_Creation' ) ) {
+			$this->_class_name = 'GF_Post_Creation';
+		}
+
+		return $this->_class_name;
 	}
 }
 
