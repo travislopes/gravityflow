5456a2e881b7a54a5e4eb291befc9be61b29fb8e
Steve Henty <stevehenty@gmail.com>
2018-12-15 15:51:06 +0100

    Update chrome

--- tests/acceptance-tests/start-local.sh
+++ tests/acceptance-tests/start-local.sh
@@ -7,7 +7,7 @@ cd ../../
 # // @group myGroup
 #docker-compose run --rm codeception run -g myGroup -vvv --html
 
-docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm --name chrome selenium/standalone-chrome-debug:3.141.59-bismuth
+docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm --name chrome selenium/standalone-chrome-debug:3.141.59-dubnium
 
 # Run just one test
 #./vendor/bin/codecept run -c codeception-local.yml tests/acceptance-tests/acceptance/0034ApprovalRevertCept.php -vvv --html
