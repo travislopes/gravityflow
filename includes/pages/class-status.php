eed7b7ccf992d5432daae60d2ca0342db04b3a6a
Jamie O <Idealien@users.noreply.github.com>
2019-02-02 10:54:33 -0500

    Search criteria (#200)
    * Added filter for search_criteria on status page - gravityflow_search_criteria_status filter

--- includes/pages/class-status.php
+++ includes/pages/class-status.php
@@ -1600,6 +1600,17 @@ class Gravity_Flow_Status_Table extends WP_List_Table {
 
 		$search_criteria = $this->get_search_criteria();
 
+		/**
+		 * Allows search_criteria to be adjusted to define which forms' entries are displayed in status table.
+		 *
+		 * Return an array of search_criteria for use with GFAPI.
+		 *
+		 * @since 2.5
+		 *
+		 * @param array   $search_criteria The search criteria
+		 */
+		$search_criteria = apply_filters( 'gravityflow_search_criteria_status', $search_criteria );
+
 		$orderby = ( ! empty( $_REQUEST['orderby'] ) ) ? $_REQUEST['orderby'] : 'date_created';
 
 		$order = ( ! empty( $_REQUEST['order'] ) ) ? $_REQUEST['order'] : 'desc';
