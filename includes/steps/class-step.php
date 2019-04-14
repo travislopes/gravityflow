74d104aff3a628bb32eb61530833f242f0cdb073
Steve Henty <stevehenty@users.noreply.github.com>
2019-04-14 12:37:24 +0200

    Fix notice when evaluating conditional routing (#238)

--- includes/steps/class-step.php
+++ includes/steps/class-step.php
@@ -1994,7 +1994,7 @@ abstract class Gravity_Flow_Step extends stdClass {
 		} else {
 			$source_field   = GFFormsModel::get_field( $form, $field_id );
 			$field_value    = empty( $entry ) ? GFFormsModel::get_field_value( $source_field, array() ) : GFFormsModel::get_lead_field_value( $entry, $source_field );
-			if ( $source_field->type == 'post_category' ) {
+			if ( $source_field && $source_field->type == 'post_category' ) {
 				// Post category values are in the format [name]:[id] e.g. cat-1:1 but GFFormsModel::is_value_match() expects just the category ID.
 				$ary                   = explode( ':', $routing_rule['value'] );
 				$routing_rule['value'] = $ary[1];
