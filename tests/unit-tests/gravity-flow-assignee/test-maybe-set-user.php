72a2990dbe924c1d15f2ddd8ef3e8ce6a6b8b774
Richard Wawrzyniak <richard@w8k.dev>
2019-12-01 09:26:36 +0000

    Add Gravity_Flow_Assignee::maybe_set_user() tests

---
+++ tests/unit-tests/gravity-flow-assignee/test-maybe-set-user.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Testing Gravity_Flow_Assignee::maybe_set_user().
+ *
+ * @group testsuite
+ */
+class Tests_Gravity_Flow_Assignee_Maybe_Set_User extends GF_UnitTestCase {
+
+	public function test_type_user_id() {
+		$assignee = $this->createMock( 'Gravity_Flow_Assignee' );
+		$assignee->method( 'get_type' )->willReturn( 'user_id' );
+		$assignee->method( 'get_id' )->willReturn( 1 );
+
+		$this->_invoke_private_method( 'Gravity_Flow_Assignee', 'maybe_set_user', array(), $assignee );
+
+		$expected_user = get_userdata( 1 );
+		$this->assertEquals( $expected_user, $this->_get_property_value( 'Gravity_Flow_Assignee', 'user', $assignee ) );
+	}
+
+	public function test_type_email() {
+		$assignee = $this->createMock( 'Gravity_Flow_Assignee' );
+		$assignee->method( 'get_type' )->willReturn( 'email' );
+		$assignee->method( 'get_id' )->willReturn( 'admin@example.org' );
+
+		$this->_invoke_private_method( 'Gravity_Flow_Assignee', 'maybe_set_user', array(), $assignee );
+
+		$expected_user = get_userdata( 1 );
+		$this->assertEquals( $expected_user, $this->_get_property_value( 'Gravity_Flow_Assignee', 'user', $assignee ) );
+	}
+
+	public function test_type_other() {
+		$assignee = $this->createMock( 'Gravity_Flow_Assignee' );
+		$assignee->method( 'get_type' )->willReturn( 'other' );
+
+		// Shouldn't be called for this type.
+		$assignee->method( 'get_id' )->willReturn( 1 );
+
+		$this->_invoke_private_method( 'Gravity_Flow_Assignee', 'maybe_set_user', array(), $assignee );
+		$this->assertNull( $this->_get_property_value( 'Gravity_Flow_Assignee', 'user', $assignee ) );
+	}
+
+	public function test_get_user() {
+		$assignee = $this->createMock( 'Gravity_Flow_Assignee' );
+		$assignee->method( 'get_user' )->willReturn( 'the user' );
+
+		// Shouldn't be called since get_user returned a value.
+		$assignee->method( 'get_type' )->willReturn( 'user_id' );
+		$assignee->method( 'get_id' )->willReturn( 1 );
+
+		$this->_invoke_private_method( 'Gravity_Flow_Assignee', 'maybe_set_user', array(), $assignee );
+		$this->assertNull( $this->_get_property_value( 'Gravity_Flow_Assignee', 'user', $assignee ) );
+	}
+
+}
