48091b38a78a6a8702df5a0f8db23054fbee2d78
Richard Wawrzyniak <richard@w8k.dev>
2020-01-04 10:38:23 +0000

    Add Gravity_Flow_Assignee::remove() unit tests

---
+++ tests/unit-tests/gravity-flow-assignee/test-remove.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Testing Gravity_Flow_Assignee::remove().
+ *
+ * @group testsuite
+ */
+class Tests_Gravity_Flow_Assignee_Remove extends GF_UnitTestCase {
+
+	public function setUp() {
+		if ( version_compare( phpversion(), '5.3', '<' ) ) {
+			$this->markTestSkipped();
+		}
+
+		parent::setUp();
+	}
+
+	public function test_status() {
+		$meta_key = 'workflow_test-type_test-id';
+		gform_add_meta( 5, $meta_key, 'testing', 1 );
+
+		$step = $this->getMockBuilder( 'Gravity_Flow_Step' )->getMock();
+		$step->method( 'get_entry_id' )->willReturn( 5 );
+
+		$assignee = new Gravity_Flow_Assignee( 'test-type|test-id', $step );
+		$assignee->remove();
+		$this->assertFalse( gform_get_meta( 5, $meta_key ) );
+	}
+
+	public function test_timestamp() {
+		$meta_key = 'workflow_test-type_test-id_timestamp';
+		gform_add_meta( 5, $meta_key, 'testing', 1 );
+
+		$step = $this->getMockBuilder( 'Gravity_Flow_Step' )->getMock();
+		$step->method( 'get_entry_id' )->willReturn( 5 );
+
+		$assignee = new Gravity_Flow_Assignee( 'test-type|test-id', $step );
+		$assignee->remove();
+		$this->assertFalse( gform_get_meta( 5, $meta_key ) );
+	}
+
+	public function test_reminder_timestamp() {
+		$meta_key = 'workflow_test-type_test-id_reminder_timestamp';
+		gform_add_meta( 5, $meta_key, 'testing', 1 );
+
+		$step = $this->getMockBuilder( 'Gravity_Flow_Step' )->getMock();
+		$step->method( 'get_entry_id' )->willReturn( 5 );
+
+		$assignee = new Gravity_Flow_Assignee( 'test-type|test-id', $step );
+		$assignee->remove();
+		$this->assertFalse( gform_get_meta( 5, $meta_key ) );
+	}
+
+}
