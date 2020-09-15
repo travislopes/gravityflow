7e816d5cc1b4fd700e9e5b4ce1e8995bd021e873
Saif Sultan <saif@gravityflow.io>
2020-09-15 19:43:08 +0530

    Added filter to remove inbox count (#354)

--- class-gravity-flow.php
+++ class-gravity-flow.php
@@ -5989,6 +5989,17 @@ jQuery('#setting-entry-filter-{$name}').gfFilterUI({$filter_settings_json}, {$va
 		 * @param array $menu The current WP Dashboard Menu.
 		 */		
 		public function show_inbox_count( $menu ) {
+
+			/**
+			 * Allows the gravityflow inbox count display to be enabled or disabled
+			 *
+			 * @param bool show Whether to show inbox count.
+			 */			
+			$show = apply_filters( 'gravityflow_inbox_count_display', true );
+			if ( ! $show ) {
+				return $menu;
+			}
+
 			$custom_labels = get_option( 'gravityflow_app_settings_labels', array() );
 			$custom_navigation_labels = rgar( $custom_labels, 'navigation' );
 			$custom_workflow_label = rgar( $custom_navigation_labels, 'workflow' );
