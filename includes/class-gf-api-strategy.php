789fcb2460fc05239e2502a6318fb3c5887fffbc
speerface <aaron@rocketgenius.com>
2021-02-21 13:06:51 -0600

    Add in specific methods for the strategy; update tests

--- includes/class-gf-api-strategy.php
+++ includes/class-gf-api-strategy.php
@@ -4,20 +4,20 @@ class Gravity_Flow_GF_API_Strategy {
 
 	private static $instance;
 
-	public function __call( $name, $arguments ) {
-		if ( ! method_exists( 'GFAPI', $name ) ) {
-			throw new \InvalidArgumentException( 'Method GFAPI::' . $name . ' not found.' );
-		}
-
-		return call_user_func_array( array( 'GFAPI', $name ), $arguments );
-	}
-
 	public function get_form( $form_id ) {
 		return GFAPI::get_form( $form_id );
 	}
 
 	public function get_entry( $entry_id ) {
 		return GFAPI::get_entry( $entry_id );
+	}
+
+	public function __call( $name, $arguments ) {
+		if ( ! method_exists( 'GFAPI', $name ) ) {
+			throw new \InvalidArgumentException( 'Method GFAPI::' . $name . ' not found.' );
+		}
+
+		return call_user_func_array( array( 'GFAPI', $name ), $arguments );
 	}
 
 	public static function instance() {
--- tests/unit-tests/test-entry-detail-page.php
+++ tests/unit-tests/test-entry-detail-page.php
@@ -10,8 +10,8 @@ class Tests_Gravity_Flow_Entry_Detail_Page extends GF_UnitTestCase {
 	private $api_stub;
 
 	public function setUp() {
-		require_once( dirname(__FILE__) . '/../../includes/pages/class-entry-detail-page.php' );
-		require_once( dirname(__FILE__) . '/../../includes/class-gf-api-strategy.php' );
+		require_once( dirname( __FILE__ ) . '/../../includes/pages/class-entry-detail-page.php' );
+		require_once( dirname( __FILE__ ) . '/../../includes/class-gf-api-strategy.php' );
 
 		$this->api_stub = $this->createMock( Gravity_Flow_GF_API_Strategy::class );
 		parent::setUp();
@@ -37,16 +37,49 @@ class Tests_Gravity_Flow_Entry_Detail_Page extends GF_UnitTestCase {
 
 	public function is_valid_data_provider() {
 		return array(
+			// Matching form IDs
 			array(
 				1,
 				2,
-				[
+				array(
 					'id' => 1,
-				],
-				[
+				),
+				array(
 					'form_id' => 1,
-				],
-				true
+				),
+				true,
+			),
+			// Non-matching form IDs
+			array(
+				1,
+				2,
+				array(
+					'id' => 2,
+				),
+				array(
+					'form_id' => 1,
+				),
+				false,
+			),
+			// Invalid/empty Form
+			array(
+				1,
+				2,
+				false,
+				array(
+					'form_id' => 1,
+				),
+				false,
+			),
+			// Invalid Entry
+			array(
+				1,
+				2,
+				array(
+					'id' => 2,
+				),
+				new \WP_Error( 'Invalid' ),
+				false,
 			),
 		);
 	}
