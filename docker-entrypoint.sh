acfb9e8ec2b8798adc4a00b73eff2ba11ce99c4a
Steve Henty <stevehenty@gmail.com>
2021-03-13 16:00:28 +0100

    Update db user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -34,7 +34,7 @@ if ! $(wp-su core is-installed); then
     wp-su core install --url=wordpress --title=tests --admin_user=admin --admin_email=test@test.com
 
     # The development version of Gravity Flow requires SCRIPT_DEBUG
-    wp-su core config --dbhost=mysql --dbname=wordpress --dbuser=root --dbpass=wordpress --extra-php="define( 'SCRIPT_DEBUG', true );" --force
+    wp-su core config --dbhost=mysql --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --extra-php="define( 'SCRIPT_DEBUG', true );" --force
 
 fi
 
